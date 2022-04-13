171e3696e31ba72950d2e3086a80129776c80bd1
Steve Gaita <steveggaita@gmail.com>
2022-04-13 14:01:23 +0300

    setup featured component in home

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
 export * from "./sidebar/Sidebar"
 export * from "./navbar/Navbar"
 export * from "./widgets/Widgets"
+export * from "./chart/Chart"
+export * from "./featured/Featured"
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,27 +1,29 @@
 import React from 'react'
 import "./home.scss"
-import { Sidebar, Navbar,Widgets} from "../../components"
+import { Sidebar, Navbar, Widgets,Chart, Featured } from "../../components"
 
 const Home = () => {
   return (
     <div className="home">
       <Sidebar />
       <div className="homeContainer">
-        <Navbar/>
+        <Navbar />
         <div className="widgets">
           {/*Edit widgets in components/widget DIR*/}
-          <Widgets type="user"/>
-          <Widgets type="order"/>
-          <Widgets type="earnings"/>
-          <Widgets type="balance"/>
+          <Widgets type="user" />
+          <Widgets type="order" />
+          <Widgets type="earnings" />
+          <Widgets type="balance" />
         </div>
 
+        {/*charts*/}
         <div className="charts">
-          
+          <Featured/>
+          <Chart/>
         </div>
       </div>
     </div>
-    
+
   )
 }
 
