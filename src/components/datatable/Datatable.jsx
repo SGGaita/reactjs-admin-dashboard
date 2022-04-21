b80f7754a6aebf02f1b8be18a238a2ba399c7065
Steve Gaita <steveggaita@gmail.com>
2022-04-21 20:33:15 +0300

    Data delete setup

--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./datatable.scss"
 import { DataGrid } from '@mui/x-data-grid';
 import { userColumns, userRows } from '../../data/datatableSource'
 import { Link } from "react-router-dom";
-import { useState } from "react";
+
 
 
 const Datatable = () => {
+  const [data, setData] = useState(userRows)
+
+  const handleDelete = (id) => {
+    setData(data.filter(item => item.id !== id))
+  }
 
   const actionColumn = [{
-    field: "action", headerName: "Actions", sortable: false, width: 200, renderCell: () => {
+    field: "action", headerName: "Actions", sortable: false, width: 200, renderCell: (params) => {
       return (
         <div className="cellAction">
           <Link to="/users/test" style={{ textDecoration: "none" }}>
@@ -17,6 +22,7 @@ const Datatable = () => {
           </Link>
           <div
             className="deleteButton"
+            onClick={() => handleDelete(params.row.id)}
           >
             Delete
           </div>
@@ -34,7 +40,7 @@ const Datatable = () => {
       </div>
       <DataGrid
         className="datagrid"
-        rows={userRows}
+        rows={data}
         columns={userColumns.concat(actionColumn)}
         pageSize={10}
         rowsPerPageOptions={[10]}
